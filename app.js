f34af06fafe4cc70274ca966d1bf6d13963ea096
mdhinesh <mdhinesh@gmail.com>
2021-10-09 23:14:22 +0530

    primitive data types

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log(typeof 'hello world')
+console.log(typeof 'h')
+console.log(typeof "hello world")
+
+console.log(typeof 20)
+console.log(typeof 20.00)
+console.log(typeof 20.40)
+
+console.log(typeof true)
+console.log(typeof false)
