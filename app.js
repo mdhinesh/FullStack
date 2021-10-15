0e0540fb856fad365529c96af614c958acae880a
mdhinesh <mdhinesh@gmail.com>
2021-10-15 14:16:27 +0530

    objects and promises

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-let car = {
-  name: "C class",
-  manufacturer: "Mercedes",
-  printt: () => {
-    console.log(`${car.name} was created by ${car.manufacturer}`)
-  }
-} 
+// let car = {
+//   name: "C class",
+//   manufacturer: "Mercedes",
+//   printt: () => {
+//     console.log(`${car.name} was created by ${car.manufacturer}`)
+//   }
+// } 
 
-console.log(car)
+// console.log(car)
 
-console.log(car.hasOwnProperty('name'))
+// console.log(car.hasOwnProperty('name'))
 
-// console.log(Object.values(car))
-// console.log(Object.keys(car))
+// // console.log(Object.values(car))
+// // console.log(Object.keys(car))
+
+var data = fetch('https://jsonplaceholder.typicode.com/todos/10')
+
+console.log(data)
