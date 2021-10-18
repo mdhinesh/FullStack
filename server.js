b9dea8d7cd190169c01e61cb7496e1345b09e88e
mdhinesh <mdhinesh@gmail.com>
2021-10-18 22:51:31 +0530

    post method

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const express = require('express')
 const PORT = 3000
 const app = express()
 
-const verify = (req,res,next)=>{
-  if(req.headers['user-agent'] === "Thunder Client (https://www.thunderclient.io)") next()
-  else res.send("BLOCKED")
-}
+app.use(express.json())
 
-app.get('/', verify, (req, res) =>{
-  res.send("VERIFIED")
+app.post('/signup', (req, res) =>{
+  console.log(req.body)
+  res.send('hiiiii')
 })
 
 app.listen(PORT,()=>{
