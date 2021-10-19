0228c7dbebdf0baccb5cd4829e0285e04614f45e
mdhinesh <mdhinesh@gmail.com>
2021-10-19 17:39:18 +0530

    E-commerce application

--- server.js
+++ server.js
@@ -1,14 +1,62 @@
 const express = require('express')
-const PORT = 3000
 const app = express()
+const PORT = 3000
 
 app.use(express.json())
 
-app.post('/signup', (req, res) =>{
-  console.log(req.body)
-  res.send('hiiiii')
+const token = "TOP_SECRET"
+let products = [{name:'iphone 12',price:'999'}, {name:'iphone11', price:'899'}, {name:'iphone10', price:'799'}]
+
+//-----validation middleware---------
+const validator = (req,res,next) =>{
+  const{name,price} = req.body
+
+  if(!name || !price)res.json({error: "validation failed"})
+  else  next()
+}
+
+//------AUTHORIZATION MIDDLEWARE----------
+const isAuthorised = (req,res,next) => {
+  if(req.headers.authorization === token) next()
+  else res.json({error: "Unauthorized"})
+}
+
+//----------------Public Route--------------
+// GET route
+// This will send products
+app.get('/products', (req,res) =>{
+  res.json({products})
 })
 
-app.listen(PORT,()=>{
-  console.log(`Server running at port ${PORT}`)
+//----------------Private Route--------------
+// POST route
+// This will add the products
+app.post('/products/add', isAuthorised, validator, (req,res) =>{
+  const{name,price} = req.body
+
+  products.push({
+    name,
+    price
+  })
+  res.send('product added')
+  }
+)
+
+//-------------AUTH ROUTES-----------
+app.post('/auth', (req,res) =>{
+  const{email,password} = req.body
+  if(email === 'admin@gmail.com' && password === 'password'){
+    res.send({token})
+  }
+    else{
+      res.send({message:"UNAUTHORIZED"})
+    }
+  })
+
+app.post('/delete', (req,res) => {
+  
+})
+
+app.listen(PORT, ()=>{
+  console.log(`Server is started in the ${PORT}`)
 })
